e240f6f04efc6d8f1f9b003b3279b240da60c0c7
SANTOSH <santosh.bharadbhunje@tekphor.com>
2024-07-25 23:23:45 +0530

    Folder Created

---
+++ app/src/main/java/com/example/checking_branches/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.checking_branches.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.checking_branches.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/checking_branches/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.checking_branches.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.checking_branches.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
